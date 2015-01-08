b83ad7c022fdeda129fdbed3cb30a92d71c11714
Zach Abrahams <zachabrahams@gmail.com>
2015-01-08 09:31:15 -0500

    Adds Feed#update_entries.

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -26,4 +26,29 @@ class Feed < ActiveRecord::Base
     self.save && self.entries.create(entries)
   end
 
+  def update_entries!
+    currEntries = Feedjira::Feed.fetch_and_parse(self.url).entries
+    currEntries = currEntries[0...40] if currEntries.length > 40
+    oldest = currEntries.last
+
+    self.entries.where("published_at < ?", oldest.published).delete_all
+
+    newEntries = currEntries.select do |currEntry|
+      currEntry.published > self.updated_at
+    end
+
+    newEntries = newEntries.map do |entry|
+      {
+        guid: entry.entry_id,
+        title: entry.title,
+        link: entry.url,
+        published_at: entry.published,
+        json: entry.to_json
+      }
+    end
+    
+    self.entries.create(newEntries)
+    self.touch
+  end
+
 end

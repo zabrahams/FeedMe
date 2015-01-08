64184f22bd021c9eb959fe73796cb5963c33876e
Zach Abrahams <zachabrahams@gmail.com>
2015-01-08 14:08:25 -0500

    Adds Feed#remove

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -1,5 +1,8 @@
 class FeedsController < ApplicationController
 
+  before_action :set_feed, only: [:show, :remove]
+  before_action :require_subscription, only: [:show, :remove]
+
   def index
     @feeds = current_user.feeds
 
@@ -25,12 +28,28 @@ class FeedsController < ApplicationController
   end
 
   def show
-    @feed = Feed.find(params[:id])
-    if current_user.feeds.include?(@feed)
-      @feed.update_entries!  # remove bang for production
-      @entries = @feed.entries.order(published_at: :desc)
-      render :show
+    @feed.update_entries!  # remove bang for production
+    @entries = @feed.entries.order(published_at: :desc)
+    render :show
+  end
+
+  def remove
+    if @feed.user_feeds.size == 1
+      @feed.destroy
     else
+      current_user.feeds.delete(@feed)
+    end
+    redirect_to feeds_url
+  end
+
+  private
+
+  def set_feed
+    @feed = Feed.find(params[:id])
+  end
+
+  def require_subscription
+    unless current_user.feeds.include?(@feed)
       flash[:errors] = ["You don't subscribe to that feed."]
       redirect_to feeds_url
     end

b0932494b37043ac2c9228f0373830203fed176e
Zach Abrahams <zachabrahams@gmail.com>
2015-01-09 16:46:51 -0500

    Creates aggregate views for categories.

--- app/assets/javascripts/views/entries/entry_show.js
+++ app/assets/javascripts/views/entries/entry_show.js
@@ -3,7 +3,6 @@ FeedMe.Views.EntryShow = Backbone.View.extend({
   template: JST['entries/show'],
 
   render: function () {
-    console.log("hi")
     this.$el.html(this.template({ entry: this.model }));
     return this;
   }

d26db41dcb7a3486804d0d68cdd7a792377534c3
Zach Abrahams <zachabrahams@gmail.com>
2015-01-09 11:45:31 -0500

    Adds EntryShow view and prepares Entry List to be made into a composite views.

--- app/assets/javascripts/views/entries/entry_list.js
+++ app/assets/javascripts/views/entries/entry_list.js
@@ -2,12 +2,31 @@ FeedMe.Views.EntryList = Backbone.View.extend({
 
   tagName: "ul",
 
+  events: {
+    "click a.entry-title": "toggleEntry"
+  },
+
   template: JST['entries/list'],
 
   render: function () {
-    console.log(this.collection);
     this.$el.html(this.template({ entries: this.collection }));
     return this;
+  },
+
+  toggleEntry: function (event) {
+    event.preventDefault()
+
+    var $title = $(event.currentTarget);
+    if ($title.hasClass("been-opened")) {
+      var $article = $title.siblings("article.entry");
+      $article.toggleClass("open closed")
+    } else {
+      $title.addClass("been-opened");
+      $article = $("<article>")
+        .addClass("entry open")
+        .html("I am an article!")
+      $title.parent().append($article);
+    }
   }
 
 });

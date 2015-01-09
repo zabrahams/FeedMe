15523cdc099b6cc492eb8d02b8adc329c0647907
Zach Abrahams <zachabrahams@gmail.com>
2015-01-09 16:34:29 -0500

    Adds a Backbone model and collection for category. Creates a categoryIndex view and template.

--- app/assets/javascripts/feed_me.js
+++ app/assets/javascripts/feed_me.js
@@ -6,6 +6,8 @@ window.FeedMe = {
   initialize: function() {
     this.feeds = new FeedMe.Collections.Feeds();
     this.feeds.fetch();
+    this.categories = new FeedMe.Collections.Categories()
+    this.categories.fetch();
     new FeedMe.Routers.Router ({
       $mainEl: $("main")
     })

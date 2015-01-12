960dcd5b2d35f59d5c0412c9abd12e9c05727733
Zach Abrahams <zachabrahams@gmail.com>
2015-01-12 16:43:48 -0500

    Completes resqueue system.

--- app/assets/javascripts/collections/entries.js
+++ app/assets/javascripts/collections/entries.js
@@ -2,5 +2,16 @@ FeedMe.Collections.Entries = Backbone.Collection.extend({
 
   model: FeedMe.Models.Entry,
 
-  url: "/api/entries"
-})
+  url: "/api/entries",
+
+  parse: function (resp) {
+    var updating = resp.updating;
+    
+    if (updating === true) {
+      window.setTimeout(this.fetch.bind(this), 4000);
+    }
+
+
+    return resp.entries;
+  }
+});
--- app/views/api/entries/index.json.jbuilder
+++ app/views/api/entries/index.json.jbuilder
@@ -1,1 +1,9 @@
-json.partial! 'api/shared/entry_list', entries: @entries
+json.entries do
+  json.partial! 'api/shared/entry_list', entries: @entries
+end
+
+if @feeds.any? { |feed| feed.updated_at > 30.seconds.ago }
+  json.updating false
+else
+  json.updating true
+end

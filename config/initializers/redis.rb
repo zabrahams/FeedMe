44cbc170a412a89066d6b25ac6d49002232aa6c4
Zach Abrahams <zachabrahams@gmail.com>
2015-01-13 20:24:22 -0500

    uncomments out redis config.

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,3 +1,3 @@
-# uri = URI.parse(ENV["REDISTOGO_URL"])
-# REDIS = Redis.new(url: ENV['REDISTOGO_URL'])
-# Resque.redis = REDIS
+uri = URI.parse(ENV["REDISTOGO_URL"])
+REDIS = Redis.new(url: ENV['REDISTOGO_URL'])
+Resque.redis = REDIS

277f6430e7338fb4ccd4460126813617cd229ee5
Zach Abrahams <zachabrahams@gmail.com>
2015-01-14 21:46:54 -0500

    Fixes bug where article closes itself.

--- app/assets/javascripts/views/entries/entry_list.js
+++ app/assets/javascripts/views/entries/entry_list.js
@@ -2,7 +2,8 @@ FeedMe.Views.EntryList = Backbone.ListView.extend({
 
   initialize: function () {
     Backbone.ListView.prototype.intialize.call(this);
-    this.listenTo(this.collection, "add remove reset sync", this.render)
+    this.listenTo(this.collection, "add remove reset sync", this.render);
+    this._openEntries = [];
   },
 
   tagName: "ul",
@@ -17,29 +18,57 @@ FeedMe.Views.EntryList = Backbone.ListView.extend({
 
   render: function () {
     this.$el.html(this.template({ entries: this.collection }));
+    this._openEntries.forEach(this._keepEntryOpen.bind(this));
     this.renderElems();
     return this;
   },
 
   toggleEntry: function (event) {
     var $title, $article, entryId, entry;
-    event.preventDefault()
+    event.preventDefault();
 
     $title = $(event.currentTarget);
     $article = $title.siblings("article.entry");
+    entryId = $article.data("id");
 
-    $article.toggleClass("open closed")
+    $article.toggleClass("open closed");
+    this._toggleOpenEntry(entryId);
+
 
     if (!$title.hasClass("been-opened")) {
       $title.addClass("been-opened");
-      entryId = $article.data("id");
       entry = this.collection.get(entryId);
-      this.addElemView($article, entry)
-
+      this.addElemView($article, entry);
       $.ajax("/api/entries/" + entryId + "/read", {
         type: "POST"
       });
     }
+  },
+
+  _toggleOpenEntry: function (entryId) {
+    var entryIndex;
+
+    entryIndex = this._openEntries.indexOf(entryId);
+    if (entryIndex >= 0) {
+      this._openEntries.splice(entryIndex, 1);
+    } else {
+      this._openEntries.push(entryId);
+    }
+  },
+
+  _keepEntryOpen: function (entryId) {
+    console.log("trying to keep it open");
+    console.log(this._openEntries);
+    var entry, $article, $title;
+
+    $article = $("#article-entry-" + entryId);
+    $article.removeClass("closed");
+    $title = $("#entry-title-" + entryId);
+    entry = this.collection.get(entryId);
+    console.log(this.collection);
+    console.log(entry);
+    this.addElemView($article, entry);
+    $title.addClass("been-opened");
   }
 
 });

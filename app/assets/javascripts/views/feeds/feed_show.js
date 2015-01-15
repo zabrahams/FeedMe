b953de57d456915a249d99b7715938a87b34ed1f
Zach Abrahams <zachabrahams@gmail.com>
2015-01-14 21:46:54 -0500

    Fixes bug where article closes itself.

--- app/assets/javascripts/views/feeds/feed_show.js
+++ app/assets/javascripts/views/feeds/feed_show.js
@@ -1,12 +1,9 @@
 FeedMe.Views.FeedShow = Backbone.View.extend({
 
-  initialize: function () {
-    this.listenTo( this.model, "sync", this.render );
-  },
-
   template: JST['feeds/show'],
 
   render: function () {
+    console.log("rendering from Feed");
     this.$el.html(this.template({ feed: this.model }));
     var entryList = new FeedMe.Views.EntryList({ collection: this.model.entries() });
     this._swapEntryList(entryList);
